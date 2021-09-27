cbf336e151eb2c6f69c29cb9ee4a603ae1ad4df5
Sayak <sayak649@gmail.com>
2021-09-28 00:32:25 +0530

    ques added

--- recursion/findProfession.js
+++ recursion/findProfession.js
@@ -1,3 +1,29 @@
+// Find profession in a special family
+
+// Consider a special family of Engineers and Doctors with following rules :
+
+// Everybody has two children.
+// First child of an Engineer is an Engineer and second child is a Doctor.
+// First child of an Doctor is Doctor and second child is an Engineer.
+// All generations of Doctors and Engineers start with Engineer.
+// We can represent the situation using below diagram:
+
+//                 E
+//            /
+//           E          D
+//         /          /
+//        E     D     D    E
+//       /    /    /    /
+//      E   D D   E  D  E  E  D
+// Given level and position of a person in above ancestor tree, find the profession of the person.
+// Examples :
+
+// Input : level = 4, pos = 2
+// Output : Doctor
+
+// Input : level = 3, pos = 4
+// Output : Engineer
+
 function findProffesion(level, pos) {
   // Base case
   if (level == 1) return "e";
